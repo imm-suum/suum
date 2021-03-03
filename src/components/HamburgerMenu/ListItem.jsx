60b2a90ff3a51f565cf530b08d22a14a3a76598d
leedahw <71396271+leedahw@users.noreply.github.com>
2021-03-03 04:55:12 -0500

    created ListItem jsx & rendered into appjs with basic styling to be changed

---
+++ src/components/HamburgerMenu/ListItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ListItem.css';
+
+const listItem = ()=>{
+    return (
+        <div className="ListItem">
+
+            <p>ListItem Label</p>
+            
+        </div>
+    )
+}
+
+export default listItem;
