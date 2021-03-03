cc482ea6bec1d029784a14e25b650bf8d153e2a3
leedahw <71396271+leedahw@users.noreply.github.com>
2021-03-03 04:27:34 -0500

    compoenent breakdown in HM folder

--- src/components/HamburgerMenu/HamburgerMenu.js
+++ src/components/HamburgerMenu/HamburgerMenu.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+//import ReactDOM from 'react-dom';
 import './HamburgerMenu.css';
 
-class App extends Component {
-
-
-    render() {
-        return (
-
-            //add imported classes here
-            //always have div to place component notes
-            <div>
-                <HabitCheckItem/>
-                <HabitCheckItem/>
-            
-            </div>
-            
-            
-            
-          );
+class HamburgerMenu extends useState {
+    constructor() {
+      super();
+      this.state = {
+        collapsed: false
+      };
     }
+  
+    //need method to expand the menu nav.
 
-}
+ 
+  
+    render() {
+      return (
+        <div>
+            {/* where the components elements should show */}
+        </div>
+      );
+    }
+  }
+  
+//to self-render for testing
+//  ReactDOM.render(
+//     <HabitCheckItem />,
+//     document.getElementById('root')
+//   );
 
-export default App;
+export default HamburgerMenu;
