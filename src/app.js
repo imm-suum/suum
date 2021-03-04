c22c3dbe8759eabff2530904dfe848c3b196b870
scdoja <71396074+scdoja@users.noreply.github.com>
2021-03-04 15:28:50 -0500

    Finalized SettingCard Components
    Added State to Toggles and merged components into one "page"; HAVE BUGS regarding toggles.

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 //import ReactDOM from 'react-dom';
 import './app.css';
 //import HabitCheckItem from './components/HabitCheckList/HabitCheckItem.js';
-//import MainSettingCard from './components/MainSettingCard/MainSettingCard.js';
-//import SettingCardToggle from './components/SettingCardToggle/SettingCardToggle.js';
-//import SettingCard from './components/SettingCard/SettingCard.js';
+import MainSettingCard from './components/MainSettingCard/MainSettingCard.js';
+import SettingCardToggle from './components/SettingCardToggle/SettingCardToggle.js';
+import SettingCard from './components/SettingCard/SettingCard.js';
 import SettingSocialMedia from './components/SettingSocialMedia/SettingSocial.js';
 
 //A class that shows all the components that we imported abve ^
 
-class App extends Component {
+const App = () => {
 
+	const [isToggled, setIsToggled] = useState(false);
 
-    render() {
         return (
 
             //add imported classes here
@@ -24,25 +24,39 @@ class App extends Component {
             //</div>
 
 			//<div>
-				//<MainSettingCard/>
 			//</div>
 
 			//<div>
-				//<SettingCardToggle rounded={true} />
-			//</div>
+				//<SettingCardToggle rounded={true} isToggled={isToggled} onToggle={() => setIsToggled(!isToggled)} />
+			//</div>,
 
-			//<div>
-				//<SettingCard/>
-			//</div>
+		<div>
+			<MainSettingCard/>
 
-			<div>
-				<SettingSocialMedia/>
+			<div className="SettingCard">
+				<div className="text">
+					<SettingCard/>
+				</div>
+					<SettingCardToggle rounded={true} isToggled={isToggled} onToggle={() => setIsToggled(!isToggled)} />
 			</div>
+
+			<div className="SettingCard2">
+				<div className="text2">
+					<SettingCard/>
+				</div>
+					<SettingCardToggle rounded={true} isToggled={isToggled} onToggle={() => setIsToggled(!isToggled)} />
+			</div>
+
+			<SettingSocialMedia/>
+
+		</div>
+
+			//<div>
+				//<SettingSocialMedia/>
+			//</div>
 
 
          );
     }
-
-}
 
 export default App;
--- src/components/SettingCardToggle/SettingCardToggle.js
+++ src/components/SettingCardToggle/SettingCardToggle.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 import './SettingCardToggle.css';
 import cx from "classnames";	//INSTALLED "npm install classnames"
 
-function SettingCardToggle({rounded = false}){
+function SettingCardToggle({rounded = false, isToggled, onToggle}){
 	const sliderCX = cx('slider',{
 		'rounded': rounded
 	})
 
 	return(
 		<label className="toggle">
-		<input type="checkbox" />
+		<input type="checkbox" checked={isToggled} onChange={onToggle}/>
 		<span className={sliderCX}/>
 		</label>
 	);
