db0675cce80a7c833039c971cb5a5d67e5af630d
leedahw <71396271+leedahw@users.noreply.github.com>
2021-03-04 19:43:49 -0500

    comment on burger jsx and rm app.js

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -8,11 +8,13 @@ const Burger = props => {
     return (
 
         <button onClick={props.click} className="flexbox burger-toggle">
+            {/* the three lines to rep icon */}
             <div className="burger-toggle-line" />
             <div className="burger-toggle-line" />
             <div className="burger-toggle-line" />
         </button>
     )
 }
+
 
 export default Burger;
