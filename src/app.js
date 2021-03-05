3dec94af8fc80f656053fd69fbb22a0cd1865e94
leedahw <71396271+leedahw@users.noreply.github.com>
2021-03-04 19:53:36 -0500

    restore app

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 //import ReactDOM from 'react-dom';
 import './app.css';
 //import HabitCheckItem from './components/HabitCheckList/HabitCheckItem.js';
-//import Nav from './components/HamburgerMenu/Nav.jsx';
-import Burger from './components/HamburgerMenu/Burger.jsx';
 import HamburgerMenu from './components/HamburgerMenu/HamburgerMenu.js';
 
 
