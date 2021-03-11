e20dc46b75d2d77d500d3e6e92094375e98548cb
You-you <shinyou_hou@hotmail.com>
2021-03-11 17:53:13 -0500

    added stash route
    added get stash route into serverApp.js
    
    created new file gettStash.js which is the route for Stash

---
+++ backEnd-suum/routes/getStash.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const Habit = require("../json-schema/Habit");
+
+//gets back all the tests
+// router.get("/", async (req, res) => {
+//   try {
+//      let stashValue = req.params.stashed;
+//     const stash = await Habit.filter(function(){
+//       return req.query.stashed === true;
+//     });
+//     res.json(stash);
+//   } 
+  
+  
+  
+//   catch (err) {
+//     res.json({ message: err });
+//   }
+// });
+
+
+router.get("/", async (req, res) => {
+  try {
+    // let stashValue = req.params.stashed;
+    // let stashValue=Habit.stashed; 
+    const stash = await Habit.filter(function(stashValue){
+        return stashValue==true; 
+
+    });
+    res.json(stash);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+module.exports = router;
--- backEnd-suum/serverApp.js
+++ backEnd-suum/serverApp.js
@@ -1,57 +1,57 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 var cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv/config");
-const bodyParser= require('body-parser'); //handles reading data from forms
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testAPIRouter = require('./routes/testAPI');
+const bodyParser = require("body-parser"); //handles reading data from forms
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var testAPIRouter = require("./routes/testAPI");
 var postsRoute = require("./routes/posts");
 const pushHabitRoute = require("./routes/pushHabit");
+const getStashRoute = require("./routes/getStash");
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json()); 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
 app.listen(3000);
-
 
 mongoose.connect(process.env.DB_CONNECTION, { usenewURLParser: true }, () => {
   console.log("connected to DB");
 });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 app.use("/testAPI", testAPIRouter);
 app.use("/posts", postsRoute);
 app.use("/pushHabit", pushHabitRoute);
-
+app.use("/getStash", getStashRoute);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
