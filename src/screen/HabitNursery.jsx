11ced986db93767c53d705f52902a1469eaf3a44
thisioke <seyitanoke@gmail.com>
2021-03-24 01:05:35 -0400

    Corrected file name to spell nursery correctly

---
+++ src/screen/HabitNursery.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+//import ReactDOM from 'react-dom';
+import '../app.css';
+
+import MainSettingCard from '../components/MainSettingCard/MainSettingCard.js';
+import SettingCard from '../components/SettingCard/SettingCard.js';
+import SettingSocialMedia from '../components/SettingSocialMedia/SettingSocial.js';
+
+
+//A Class that holds all components for Settings Screen
+
+class HabitNursery extends Component {
+
+
+  render() {
+	  return (
+
+		  //add imported classes here
+		  //always have div to place component notes
+			<div>
+
+				<SettingCard/>
+                <MainSettingCard/>
+
+				<SettingSocialMedia/>
+
+            </div>
+
+		);
+  }
+
+}
+
+export default HabitNursery;
