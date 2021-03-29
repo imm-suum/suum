506c7b9a43f071405fe3a3ed11782c94597de18e
thisioke <seyitanoke@gmail.com>
2021-03-29 15:25:06 -0400

    Clean up and commenting

--- src/components/HabitCheckList/HabitCheckItem.js
+++ src/components/HabitCheckList/HabitCheckItem.js
@@ -1,15 +1,9 @@
 import React, {useState} from 'react';
-//import ReactDOM from 'react-dom';
 import './HabitCheckItem.css';
 
 
 export const HabitCheckItem = ({checked, habitName, setHabitState}) => {
 
-      // const dosomething = () => {
-      //     setHabit(!checked);
-      //     // disable this checkbox
-      //     // write code to call the backend to say the item is checked
-      // }
       return (
         <div>
           {checked ? //ternary operator to display disabled checkitem or actionable one.
@@ -36,16 +30,3 @@ export const HabitCheckItem = ({checked, habitName, setHabitState}) => {
       );
 }
   
-
-
-// {!checked ? 
-//   <input type="checkbox" checked={checked} name="checkItem" onChange={() => !checked ? dosomething : null}
-//   /> : 
-//   <input type="checkbox" disabled checked={checked} name="checkItem" onChange={() => !checked ? dosomething : null}
-//   />
-// }
-
-
-// <input type="checkbox" checked={checked} name="checkItem" onChange={() => setHabit(!checked)}
-// />
-// <p className= "checkItemText">{habitName}</p>
--- src/components/HabitTabWidget/HabitTabWidget.js
+++ src/components/HabitTabWidget/HabitTabWidget.js
@@ -1,54 +1,40 @@
 import React, {useEffect, useState} from 'react';
-import './HabitTabWidget.scss';
 import { HabitCheckItem } from '../HabitCheckList/HabitCheckItem';
-
 import { Tabs } from './Tabs';
-//need habitcheckItem for content render.
+import './HabitTabWidget.scss';
 
 
-export const HabitTabWidget = () => {
+
+export const HabitTabWidget = (todayHabits) => {
+  //Track state for the Habit Tab Widget component to know what tab has been clicked so far
   const [activeTab, setActiveTab ] = useState('tab1');
+  //Create 3 Tabs by my naming them in this array
+  const tabs = ["tab1", "tab2", "tab3"]
+
+  //Track state for each of the 3 Habit Check Items that are used 
   const [ habit1IsChecked, setHabit1 ] = useState(false);
   const [ habit2IsChecked, setHabit2 ] = useState(true);
   const [ habit3IsChecked, setHabit3 ] = useState(false);
   
-  const tabs = ["tab1", "tab2", "tab3"]
-
-
+  // Create and store new stances of the habitCheckItem in here with the 
   const content = {
-    tab1: <HabitCheckItem setHabit={setHabit1} checked={habit1IsChecked} habitName= "habit 1 name"/>,
-    tab2: <HabitCheckItem setHabit={setHabit2} checked={habit2IsChecked} habitName= "habit 2 name"/>,
-    tab3: <HabitCheckItem setHabit={setHabit3} checked={habit3IsChecked} habitName= "habit 3 name"/>
+    tab1: <HabitCheckItem setHabitState={setHabit1} checked={habit1IsChecked} habitName= "habit 1 name"/>,
+    tab2: <HabitCheckItem setHabitState={setHabit2} checked={habit2IsChecked} habitName= "habit 2 name"/>,
+    tab3: <HabitCheckItem setHabitState={setHabit3} checked={habit3IsChecked} habitName= "habit 3 name"/>
   }
   
+  //When tab is clicked this function sets the state of the active tab on the HabitTabWidget to the sected tab
   const onTabClickHandler = (tab) => {
     setActiveTab(tab);
   }
+  //This function is called anytime there is a change in the activeTab. It gets the appropriate content form the content object for the active tab
   const displayContent = (tab) => {
     return content[tab];
   }
-
-  ///habitTab1 is checked && dispalay component
-  //
-
-  // async function asyncFunc() {
-  //   try {
-  //     // fetch data from a url endpoint
-  //     const data = await axios.get("/some_url_endpoint");
-  //     return data;
-  //   } catch(error) {
-  //     console.log("error", error);
-  //     // appropriately handle the error
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   asyncFunc();
-  // [asyncFunc]});
+  
 
 
   return (
-    //needs this div to house the component to show on screen.
      <div> 
         <h1>HI WE'RE TESTING THE HabitTabWidget</h1>
         <div>
@@ -56,6 +42,7 @@ export const HabitTabWidget = () => {
             tabs.map((tab, idx)=> {
               return <Tabs key={idx} onClickHandler={onTabClickHandler} tab={tab} />
             })
+
           }
         <div className='HabitTabWidget bg-green'>{displayContent(activeTab)}</div>
         </div>
