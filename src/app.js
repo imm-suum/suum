b7b605bed8480e5d9534e319cb0d1cd9ecdcd1eb
thisioke <seyitanoke@gmail.com>
2021-04-05 11:08:48 -0400

    cherry pick app.js from main

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ import WeeklyReport from './screen/WeeklyReport.jsx';
 
 import HamburgerMenu from './components/HamburgerMenu/HamburgerMenu.js';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { CheckInModal } from './components/checkInModal/checkinModal';
 
 //A class that shows all the components that we imported abve ^
 
@@ -22,9 +21,23 @@ class App extends Component {
 
   render() {
 	  return (
-			<div>
-				<CheckInModal/>
-			</div>
+		 	//Router navigates the "page" in the broswer search bar to the correct screen component as listed below
+
+			//Switch ensures that each page component is displayed on its own. And the URL has to be an 'exact match'
+			<Router>
+				
+				<Switch>
+					<Route path="/" exact component={HabitNursery}/>
+					<Route path="/weeklyReport" exact component={WeeklyReport}/>
+					<Route path="/stash" exact component={Stash}/>
+					<Route path="/history" exact component={History}/>
+					<Route path="/settings"  exact component={Settings}/>
+				</Switch>
+
+				{/* The hamburger Menu will now appear above all the pages in the app because its always rendered on the app*/}
+				<HamburgerMenu/> 
+
+			</Router>
 
 		);
   }
