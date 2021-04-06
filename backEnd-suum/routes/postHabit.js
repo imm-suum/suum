307c843aa90d40975117568e2d942b6136005efa
thisioke <seyitanoke@gmail.com>
2021-04-06 13:06:35 -0400

    Post Habit now works. The route is autenticated. It will take a new habit name in the payload and then add the new habit to the user object

--- backEnd-suum/routes/postHabit.js
+++ backEnd-suum/routes/postHabit.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const Habit = require("../json-schema/Habit");
-var mongoose = require('mongoose');
+const User = require("../json-schema/User");
 const verify = require('./verifyToken');
 
 //submits the habits
 router.post("/", verify, async (req, res) => {
-  console.log(req.body);
-  console.log(req.user._id);
+  //console.log(req.body);
+  //console.log(req.user._id);
   
   //make New user Habit
   const habit = new Habit({
@@ -16,37 +16,36 @@ router.post("/", verify, async (req, res) => {
 
   });
 
-  console.log(habit);
+  //console.log(habit);
 
-  //Try to save Habit
-  const newHabitId = {habit:""};
+ //The code below is a little bit of call back hell, but it works. I dont think 1 call back is all that bad.
   try{
-    const saveHabit = await habit.save();
-    
+    //Try to save Habit in async function
+    const saveHabit = await habit.save()
+
+      //Use updateOne() to find user for the habit based on the habit.user_id
+      .then( async data =>{
+
+        const updateUserHabitArray = await User.updateOne(
+          {_id: data.user_id}, //Find the USER document with this _id 
+          {
+            //then use $addToset to populate new Habit Id in the user object habit array
+            $addToSet:{
+              habits:[data._id]
+            }
+          }
+        )
+        //Console log for debugging the recipt of the update 
+        //console.log(updateUserHabitArray);
+        
+        //res.json(updateUserHabitArray);
+      });
+      
     res.send({habit: habit.id});
-    
   } 
   catch (err){
-
     res.status(400).send(err);
   }
-
-  console.log(newHabitId);
-  
-
-  //Try to populate new Habit Id in the approproate user object
-  // try{
-  //   // const saveHabit = await habit.save();
-  //   // res.send({habit});
-
-  //   // habit.findOne(habit.user_id)
-  // // .populate('user_id')
-  // // .exec(function(err, habit){
-  // } 
-  // catch (err){
-
-  //   res.status(400).send(err);
-  // }
 
 
 });
