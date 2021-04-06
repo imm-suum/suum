a009bff04820450cd80b4a6b0792d2a1c9937617
Alana D. Lee <leedahw@sheridancollege.ca>
2021-04-06 11:58:19 -0400

    changed NoHabitsWidget css to scss. fixed styling as per XD. added DateTime.scss cleaned up redundant selectors.

--- src/components/DateTime/DateTime.jsx
+++ src/components/DateTime/DateTime.jsx
@@ -1,11 +1,11 @@
 import { Component } from "react";
-
+import './DateTime.scss';
 
 class DateTime extends Component {
   state = {};
   render() {
     return (
-      <div className="App">
+      <div className="text-left dateTimeContainer">
         <h1>Current Date</h1>
         <h2>{this.props.date.toLocaleDateString()}</h2>
       </div>
