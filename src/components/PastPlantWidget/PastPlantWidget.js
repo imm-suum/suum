fa42a2f9455ed627ffbe4be808a50613877d529f
Alana D. Lee <leedahw@sheridancollege.ca>
2021-04-10 23:08:06 -0400

    idk...

--- src/components/PastPlantWidget/PastPlantWidget.js
+++ src/components/PastPlantWidget/PastPlantWidget.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import ReactDOM from 'react-dom';
-import './PastPlantWidget.css';
+import './PastPlantWidget.scss';
 
 class PastPlantWidget extends Component{
 	  constructor(props) {
--- src/screen/History.jsx
+++ src/screen/History.jsx
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 //import ReactDOM from 'react-dom';
 import '../app.css';
-import DateTime from '../components/DateTime/DateTime';
+import DateTime from '../components/DateTime/DateTime.jsx';
+import PastPlantWidget from '../components/PastPlantWidget/PastPlantWidget';
 
 
 //A Class that holds all components for Habit Nursery Screen
 
 class History extends Component {
+	
+	plantWidgetContainer = {
+
+	}
 
   render() {
+		//GET API response 
+
+		//for (i=0; i< response.date.month.length; i++){
+			//for (j=0; j<response.date.month.week.length; j++) {
+				//weeklyresponse = return array of objects. Objects=array of habits.
+				//map the weekly response as <PastPlantWidget weekRange={} completedProgress={} />
+			//}
+		//}
+
+
 	  return (
 
 		  //add imported classes here
 		  //always have div to place component notes
 			<div>
 				<DateTime date={new Date()} />
+				<div className="plantWidgetContainer">
+				<PastPlantWidget/>
+				</div>
       </div>
 
 		);
