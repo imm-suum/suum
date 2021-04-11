170b0faba15ba7a5fde588163b00ae763fcbe88f
Alana D. Lee <leedahw@sheridancollege.ca>
2021-04-10 22:31:19 -0400

    updated DateTime component

--- src/screen/History.jsx
+++ src/screen/History.jsx
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 //import ReactDOM from 'react-dom';
 import '../app.css';
 import DateTime from '../components/DateTime/DateTime';
-import OnBoardingScreens from '../components/OnBoardingScreens/OnBoardingScreens.js';
 
 
 //A Class that holds all components for Habit Nursery Screen
 
 class History extends Component {
-
 
   render() {
 	  return (
@@ -16,11 +14,8 @@ class History extends Component {
 		  //add imported classes here
 		  //always have div to place component notes
 			<div>
-
-				<DateTime/>
-
-				<OnBoardingScreens/>
-            </div>
+				<DateTime date={new Date()} />
+      </div>
 
 		);
   }
