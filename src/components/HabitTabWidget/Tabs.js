2a732689821e3f9ec3df6f3b605f573742b661e5
scdoja <71396074+scdoja@users.noreply.github.com>
2021-04-12 10:00:00 -0400

    Updated HabitTabWidget bgColour

--- src/components/HabitTabWidget/Tabs.js
+++ src/components/HabitTabWidget/Tabs.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Tabs.scss';
+import leafIcon from '../../assets/leafIcon.svg'
 
 export const Tabs = ({onClickHandler, tab}) => {
 
     return (
         <button className="habitTabIdx"
             onClick={() => onClickHandler(tab)}
-        >
-            {tab}
+        ><img src={leafIcon}></img>
+
         </button>
     )
 }
