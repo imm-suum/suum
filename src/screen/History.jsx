1442933ea547880e7909349a532da935676b5b16
Alana D. Lee <leedahw@sheridancollege.ca>
2021-04-11 21:46:29 -0400

    updated styling for history Screen

--- src/screen/History.jsx
+++ src/screen/History.jsx
@@ -14,10 +14,34 @@ class History extends Component {
   render() {
 			
 		const historyScreenPadding = {
+			paddingTop: "2rem",
 			paddingLeft: '2rem',
-
 		}
 
+		const plantWidgetContainer = {
+			display: "grid",
+			gridGap: "3rem",
+			scrollDirection:"smooth",
+			gridTemplateColumns: "repeat(5, calc(50% - 40px))",
+  		gridTemplateRows: "minmax(150px, 1fr)",
+			overflowX:"scroll",
+			marginBottom:"3rem"
+		}
+
+		const pastPlantTitle={
+			fontSize: "24px",
+			fontWeight: "600",
+			color: "#323232",
+			marginBottom: "4rem"
+		}
+
+
+		const monthTitle = {
+			fontSize: "18px",
+			fontWeight: "400",
+			color: "#323232",
+			marginBottom: ".5rem"
+		}
 		//GET API response 
 
 		//for (i=0; i< response.date.month.length; i++){
@@ -33,9 +57,25 @@ class History extends Component {
 		  //add imported classes here
 		  //always have div to place component notes
 			<div style={historyScreenPadding}>
-				<DateTime date={new Date()} />
+				<h1 style={pastPlantTitle}>Past Plants</h1>
 				
-				<div>
+				<h2 style={monthTitle}>January 2021</h2>
+				<div style={plantWidgetContainer}>
+					{/* where we will map the plast plant widget */}
+					<PastPlantWidget/>
+					<PastPlantWidget/>
+					<PastPlantWidget/>
+					<PastPlantWidget/>
+					<PastPlantWidget/>
+				</div>
+
+				<h2 style={monthTitle}>February 2021</h2>
+				<div style={plantWidgetContainer}>
+					{/* where we will map the plast plant widget */}
+					<PastPlantWidget/>
+					<PastPlantWidget/>
+					<PastPlantWidget/>
+					<PastPlantWidget/>
 					<PastPlantWidget/>
 				</div>
       </div>
