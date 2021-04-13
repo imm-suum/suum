e4ff148e11d8bd98a4833446827ed55773f6e7ef
scdoja <71396074+scdoja@users.noreply.github.com>
2021-04-12 22:57:09 -0400

    Login Form Styling

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -16,13 +16,13 @@ export const LoginForm = ()=> {
 
 async function apiCall(d) {
 	try {
-		
+
 		// fetch data from a url endpoint
 		const registerCall = await axios.post(`/api/user/register`, {
 			name: d.name ,
 			email: d.email,
 			password: d.password,
-			notifications: d.notifications   
+			notifications: d.notifications
 		  })
 		.then(res=>{
 			//redirect('/');
@@ -37,17 +37,20 @@ async function apiCall(d) {
 		// appropriately handle the error
 	}
 }
-  
+
 
 
 return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<input id="name" type="text" placeholder="Full Name" {...register("name")} />
-			<input id="email" type="email" placeholder="Email" {...register("email")} />
-			<input id="password" type="text" placeholder="Password" {...register("password")} />
-			<input id="notifications" type="checkbox" placeholder="Notifications" {...register("notifications")} />
-
-			<input type="submit" />
+		<form className="loginForm" onSubmit={handleSubmit(onSubmit)}>
+		<h1>Register</h1>
+			<input id="name" className="name" type="text" placeholder="Full Name" {...register("name")} />
+			<input id="email" className="email" type="email" placeholder="Email" {...register("email")} />
+			<input id="password" className="password" type="password" placeholder="Password" {...register("password")} />
+			<div>
+				<input id="notifications" className="notif" type="checkbox" placeholder="Notifications" {...register("notifications")} />
+				<label htmlFor="notifications" style={{marginLeft: "0.5rem"}}>Turn On Notifications</label>
+			</div>
+			<input className="submit" type="submit" />
 		</form>
   );
 }
