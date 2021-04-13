8a13139aabc14142a361c781a84324e6b6e5637c
thisioke <seyitanoke@gmail.com>
2021-04-12 20:49:47 -0400

    Added a return jwt in cookie when user registers. Also removed userid from return message

--- backEnd-suum/routes/UserEndPoint.js
+++ backEnd-suum/routes/UserEndPoint.js
@@ -63,11 +63,16 @@ router.post('/register', async (req,res) => {
         name: req.body.name,
         email: req.body.email,
         password: hashPassword,
-        notifications:req.body.notifications
+        notifications: req.body.notifications
     });
     try{
         const savedUser = await user.save();
-        res.send({user: user.id});
+
+        //Create and assign a token
+        const token = jwt.sign({ _id:user._id }, process.env.TOKEN_SECRET);
+        res.cookie('auth-token', token, {httpOnly: true});
+        //res.header('auth-token', token).send(token);
+        res.send('Succesfuly registered ');
     }catch(err){
         res.status(400).send(err);
     }
