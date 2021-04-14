d4877d96266d0ab194931cf8aa2641ab78f28256
thisioke <seyitanoke@gmail.com>
2021-04-14 14:07:12 -0400

    Stubbed the code to force it to send text messages. And it works now. We need to write our own custom logic  for timing

--- backEnd-suum/json-schema/Appointment.js
+++ backEnd-suum/json-schema/Appointment.js
@@ -13,6 +13,22 @@ const AppointmentSchema = new mongoose.Schema({
 
 //Figure out how this works >>
 AppointmentSchema.methods.requiresNotification = function (date) {
+  console.log(date, "< date that was passed in");
+  console.log(moment(date), "< date passed into moment");
+  console.log(moment(date).utc(), "< date that was passed into UTC");
+  console.log(moment(this.time).tz(this.timeZone), "< Line 30 1 no utc");
+  console.log(moment(this.time).tz(this.timeZone).utc(), "< Line 30 1");
+  console.log(moment(this.time).tz(this.timeZone).utc().diff(moment(date).utc()), "< Line 30 2");
+  console.log(moment.duration(moment(this.time).tz(this.timeZone).utc().diff(moment(date).utc())).asMinutes(), "< Line 28");
+  console.log(Math.round(moment.duration(moment(this.time).tz(this.timeZone).utc().diff(moment(date).utc())).asMinutes()), "< Line 27");
+  console.log(this.notification, "< Line 33");
+  console.log(this.timeZone, "this.timezone");
+
+  
+  //if time is Morning 
+    //for each users[] send morning-text to (user.number) were User.notifications=true
+  //if time is afternoon
+    //for each users[] send afternoon-text to (user.number) were User.notifications=true
   return (
     Math.round(
       moment
@@ -31,21 +47,25 @@ AppointmentSchema.statics.sendNotifications = function (callback) {
   Appointment.find().then(function (appointments) {
 
     console.log("found objects");
+    let appoint;
 
     appointments = appointments.filter(function (appointment) {
 
       console.log(appointment + " single object");
       console.log(appointments + "FILTERED objects");
-      //console.log(requiresNotification(searchDate));
-      console.log(appointment.requiresNotification(searchDate));
+      appoint = appointments;
+      console.log(appoint);
+      console.log(appointment.requiresNotification(searchDate), " < line 50 from appointment.requiresNotification(searchDate");
+      
 
       return appointment.requiresNotification(searchDate);
     });
-
+    
     console.log(appointments.length);
-    if (appointments.length > 0) {
-      sendNotifications(appointments);
+    if (1 > 0) {
+      sendNotifications(appoint);
       console.log("more than 1 appointment ");
+      console.log(appoint);
     }
   });
 
@@ -65,7 +85,7 @@ AppointmentSchema.statics.sendNotifications = function (callback) {
         to: `+ ${appointment.phoneNumber}`,
         from: process.env.TWILIO_PHONE_NUMBER,
         /* eslint-disable max-len */
-        body: `Hi ${appointment.name}. Just a reminder that you have an appointment coming up.`,
+        body: `Hi ${appointment.name}. Just a reminder that you have a suum habit coming up.`,
         /* eslint-enable max-len */
       };
 
