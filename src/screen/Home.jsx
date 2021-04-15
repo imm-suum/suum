949eb328b98628336c478114b083b14062861f6a
thisioke <seyitanoke@gmail.com>
2021-04-14 20:27:40 -0400

    Fixed api issue Closes #98

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -14,62 +14,74 @@ import DateTime from '../components/DateTime/DateTime.jsx';
 // fecth all habits.
 //make all components dark mode as well.
 
- ///habitTab1 is checked && dispalay component
-  //
+///habitTab1 is checked && dispalay component
+//
 
 
 
-export const Home =  () => {
+export const Home = () => {
 	let anim;
-    // get API response.
+	// get API response.
 
 	const [todayHabits, setHabits] = useState([]);
+	const [todayTip, setTodayTip] = useState([]);
 
 	useEffect(() => {
-		//apiCall();
-	});
-
-	async function apiCall() {
-		try {
-			
+		async function getHabits() {
 			// fetch data from a url endpoint
-			const data = await axios.get(`/api/habit`)
-			.then(res=>{
-				setHabits(res.data);
-			});
-			//setHabits(data.json());
-			//const items = await data.json();
-				//console.log(res);
+			const getHabit = await axios.get('/api/habit')
+				.then(response => {
+					setHabits(response.data);
+				});
+			const getTipofDay = await axios.get('/api/tips')
+				.then(response => {
+					setTodayTip(response.data);
+				});
 
-			return data;
-		} catch(error) {
-			console.log("error", error);
-			// appropriately handle the error
 		}
-	}
+		getHabits();
 
+		anim = lottie.loadAnimation({
+			container: plantAnimationDiv.current,
+			renderer: "svg",
+			loop: false,
+			autoplay: false,
+			animationData: plantAnimation
+		});
+		return () => anim.destroy(); // optional clean up for unmounting
+	}, []);
+
+	// async function apiCall() {
+	// 	try {
+
+	// 		// fetch data from a url endpoint
+	// 		const data = await axios.get(`/api/habit`, {headers: {'Content-Type':'application/json','Access-Control-Allow-Origin': '*'}})
+	// 		.then(res=>{
+	// 			setHabits(res.data);
+	// 			//console.log(todayHabits);
+	// 		});
+	// 		return data;
+	// 	} catch(error) {
+	// 		console.log("error", error);
+	// 		// appropriately handle the error
+	// 	}
+	// }
+
+	console.log(todayHabits);
+	//console.log(todayTip);
 	let plantAnimationDiv = createRef();
 
-  useEffect(() => {
-    anim = lottie.loadAnimation({
-      container: plantAnimationDiv.current,
-      renderer: "svg",
-      loop: false,
-      autoplay: false,
-      animationData: plantAnimation
-    });
-    return () => anim.destroy(); // optional clean up for unmounting
-  }, []);
 
 
-	const playAnimation = ()=>{
-		anim.playSegments([0,165], true);
+
+	const playAnimation = () => {
+		anim.playSegments([0, 165], true);
 		//loopAnimation();
 		console.log("playing")
 	}
 
-	const loopAnimation = ()=>{
-		anim.playSegments([139,165], false);
+	const loopAnimation = () => {
+		anim.playSegments([139, 165], false);
 	}
 
 	const dateTimePadding = {
@@ -77,29 +89,29 @@ export const Home =  () => {
 		paddingTop: '2rem'
 	}
 
-	  return (
-      //if night time show checkin process.
-      //IF (Date.prototype.getHours() > 20 && Date!= "friday" ) {
-      // show checkinModal => Planning Modal
-      // }else if (Date.prototype.getHours() > 20 && Date(day) === "friday") {
-      //     //show weeklyReviewCard componentONly.
-      //     //on close... load homescreen with habitTabWidget?????
-      // }else{
-          //if it's before 8pm on whatever day
-          //show habitTabWidget
-      //}
+	return (
+		//if night time show checkin process.
+		//IF (Date.prototype.getHours() > 20 && Date!= "friday" ) {
+		// show checkinModal => Planning Modal
+		// }else if (Date.prototype.getHours() > 20 && Date(day) === "friday") {
+		//     //show weeklyReviewCard componentONly.
+		//     //on close... load homescreen with habitTabWidget?????
+		// }else{
+		//if it's before 8pm on whatever day
+		//show habitTabWidget
+		//}
 
-		  //add imported classes here
-		  //always have div to place component notes
+		//add imported classes here
+		//always have div to place component notes
 
-			<div>
-        <div style={dateTimePadding}>
-          <DateTime date={new Date()} />
-        </div>
-		  	<HabitTabWidget todayHabits={todayHabits} playAnimation={playAnimation}/>
-				<button onClick={playAnimation}>Play</button>
-				<div className="plantAnimationDiv" ref={plantAnimationDiv} />
+		<div>
+			<div style={dateTimePadding}>
+				<DateTime date={new Date()} />
+			</div>
+			{todayHabits.length > 0 && <HabitTabWidget todayHabits={todayHabits} playAnimation={playAnimation} tipOfDay={todayTip} />}
+			<button onClick={playAnimation}>Play</button>
+			<div className="plantAnimationDiv" ref={plantAnimationDiv} />
 
-      </div>
-		);
+		</div>
+	);
 }
