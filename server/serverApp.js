3b81217c719ec3232bac307d1f911e5fc509b051
You-you <shinyou_hou@hotmail.com>
2021-04-15 17:27:47 -0400

    ready for deployement files

--- server/serverApp.js
+++ server/serverApp.js
@@ -86,7 +86,7 @@ if (process.env.NODE_ENV === "production") {
   // set static folder
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve_dirname, "client", "build", "index.html");
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 module.exports = app;
