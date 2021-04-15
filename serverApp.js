c6989db3d92c8dfb03f1282a6ed879b540bfca55
You-you <shinyou_hou@hotmail.com>
2021-04-15 18:28:59 -0400

    changed package.json and took everything our of server file

--- serverApp.js
+++ serverApp.js
@@ -47,6 +47,17 @@ app.listen(port, () =>
   console.log("Express server is running on localhost:5000")
 );
 
+// serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+
+
 mongoose.connect(
   process.env.DB_CONNECTION,
   { useNewUrlParser: true, useUnifiedTopology: true },
@@ -81,12 +92,5 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-// serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+
 module.exports = app;
