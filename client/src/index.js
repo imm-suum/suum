22eb0f3c93f092ea0e23562a3b0b75fd448d1ba3
thisioke <seyitanoke@gmail.com>
2021-05-06 12:16:51 -0400

    added

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 import App from './app.js';
 
 
-  ReactDOM.render(
-    <React.StrictMode>
-<div id="phoneWrapper">
+ReactDOM.render(
+  <React.StrictMode>
+    <div id="phoneWrapper">
       {/* <App /> */}
-      <iframe id="iframes" src="https://suum-app-iframe.herokuapp.com/" style={{width:"100%",height:"100%"}} />
-</div>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
+      <iframe id="iframes" src="https://suum-app-iframe.herokuapp.com/" />
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
