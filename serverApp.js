528f71e724bc1159ca819bdaaa747866492eb6a0
thisioke <seyitanoke@gmail.com>
2021-05-06 13:29:41 -0400

    added back slash

--- serverApp.js
+++ serverApp.js
@@ -89,7 +89,7 @@ app.use(function (err, req, res, next) {
 if (process.env.NODE_ENV === "production") {
   // set static folder
   // app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", 'build', 'index.html'));
   });
 }
