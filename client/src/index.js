f5acb8265bcbee11a3919423cab27e1d7547137b
thisioke <seyitanoke@gmail.com>
2021-05-06 11:54:22 -0400

    iframed from iframe app

--- client/src/index.js
+++ client/src/index.js
@@ -6,10 +6,10 @@ import App from './app.js';
 
   ReactDOM.render(
     <React.StrictMode>
-{/* <div id="phoneWrapper"> */}
-      <App />
-      {/* <iframe id="iframes" src="https://suum-app.herokuapp.com/" style={{width:"100%",height:"100%"}} /> */}
-{/* </div> */}
+<div id="phoneWrapper">
+      {/* <App /> */}
+      <iframe id="iframes" src="https://suum-app-iframe.herokuapp.com/" style={{width:"100%",height:"100%"}} />
+</div>
     </React.StrictMode>,
     document.getElementById('root')
   );
