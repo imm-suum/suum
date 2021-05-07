9daadaaabbe2540b77b01b619c6a3a40ddfd9800
thisioke <seyitanoke@gmail.com>
2021-05-07 13:39:25 -0400

    Added minmun placeholder

--- client/src/components/RegisterForm/RegisterForm.jsx
+++ client/src/components/RegisterForm/RegisterForm.jsx
@@ -34,12 +34,12 @@ return (
 	<div className="form-container">
 	<form className="suumForm" onSubmit={handleSubmit(onSubmit)}>
 	<h1 className="RegisterTitle">Register</h1>
-		<input id="name" className="name" type="text" placeholder="Full Name" {...register("name", {required:true, minLength:1, maxLength:35})} />
+		<input id="name" className="name" type="text" placeholder="Full Name (min 7 characters)" {...register("name", {required:true, minLength:1, maxLength:35})} />
 
-		<input id="tel" type="tel" className="tel" placeholder="Phone Number" {...register("phoneNumber", {required: true, minLength:6, maxLength:14 })} />
+		<input id="tel" type="tel" className="tel" placeholder="Phone Number (min 10 characters)" {...register("phoneNumber", {required: true, minLength:6, maxLength:14 })} />
 		{/* , pattern:regexConst {errors.phoneNumber?.type != "pattern" && <span className="num">Phone number in E.164 format: +123456789</span>} */}
 
-		<input id="password" className="password" type="password" placeholder="Password" {...register("password", {required:true, minLength:6, maxLength:35})} />
+		<input id="password" className="password" type="password" placeholder="Password (min 10 characters)" {...register("password", {required:true, minLength:6, maxLength:35})} />
 		{errors.password?.type === "minLength" && <span className="error">Please Input 6 Characters for Password!</span>}
 
 		<div className="notifContainer">
